03fe543f1bd693458e18a54e5ffab4a3c72160c3
Igor Filonov <zxfisxz@gmail.com>
2024-10-23 23:48:47 +0300

    TZ-6-TestsDone

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -67,25 +67,5 @@ class InMemoryHistoryManagerTest {
     }
 
     // Тест добавления нескольких задач и проверки их порядка
-    @Test
-    void testAddingMultipleTasksAndOrder() {
-        Task task1 = new Task("Task 1", "Description 1");
-        Task task2 = new Task("Task 2", "Description 2");
 
-        historyManager.add(task1);
-        historyManager.add(task2);
-
-        List<Task> history = historyManager.getHistory();
-        assertEquals(2, history.size(), "История должна содержать две задачи.");
-        assertEquals(task1, history.get(0), "Первая задача должна быть task1.");
-        assertEquals(task2, history.get(1), "Вторая задача должна быть task2.");
-
-        // Добавляем task1 ещё раз, она должна переместиться в конец
-        historyManager.add(task1);
-        history = historyManager.getHistory();
-
-        assertEquals(2, history.size(), "История должна содержать две задачи.");
-        assertEquals(task2, history.get(0), "Первая задача должна быть task2.");
-        assertEquals(task1, history.get(1), "Вторая задача должна быть task1 после повторного добавления.");
-    }
 }
